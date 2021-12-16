44a7beb9eb3c52515ac37c06678600345dfbe97f
kaamrul <mk.kamrulhasan@outllok.com>
2021-12-17 00:07:47 +0600

    Test

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Models\User;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('welcome');  
+}); 
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('admin.index');
